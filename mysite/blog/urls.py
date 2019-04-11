9bb163d448544766ce5810fbc787acc0756fc48c
1192927413@qq.com <1192927413@qq.com>
2019-04-12 00:21:21 +0800

    vision0.004

--- mysite/blog/urls.py
+++ mysite/blog/urls.py
@@ -7,4 +7,5 @@ urlpatterns = [
     url('^$', views.index, name='index'),
     url(r'^post/(?P<pk>[0-9]+)/$', views.detail, name='detail'),
     url(r'^category/(?P<pk>[0-9]+)/$', views.category, name='category'),
+    url(r'^tags/(?P<pk>[0-9]+)/$', views.tags, name='tags'),
 ]
--- mysite/blog/views.py
+++ mysite/blog/views.py
@@ -1,4 +1,5 @@
 from django.shortcuts import render,get_object_or_404
+from django.core.paginator import Paginator, EmptyPage, PageNotAnInteger
 from django.http import HttpResponse
 from .models import Post,Category,Tag
 from comments.forms import CommentForm
@@ -7,7 +8,20 @@ from comments.forms import CommentForm
 # Create your views here.
 def index(request):
     post_list = Post.objects.all().order_by('-created_time')
-    return render(request, 'blog/index.html', context={'post_list': post_list})
+    paginator = Paginator(post_list, 5)
+
+    page = request.GET.get('page')
+
+    try:
+        contacts = paginator.page(page)
+
+    except PageNotAnInteger:
+        contacts = paginator.page(1)
+
+    except EmptyPage:
+        contacts = paginator.page(paginator.num_pages)
+    return render(request, 'blog/index.html', context={'contacts': contacts})
+
 
 
 def detail(request,pk):
@@ -21,7 +35,38 @@ def detail(request,pk):
                }
     return render(request, 'blog/detail.html', context=context)
 
+
 def category(request, pk):
     cate = get_object_or_404(Category, pk=pk)
     post_list = Post.objects.filter(category=cate).order_by('-created_time')
-    return render(request, 'blog/index.html', context={'post_list': post_list})
+    paginator = Paginator(post_list, 5)
+
+    page = request.GET.get('page')
+
+    try:
+        contacts = paginator.page(page)
+
+    except PageNotAnInteger:
+        contacts = paginator.page(1)
+
+    except EmptyPage:
+        contacts = paginator.page(paginator.num_pages)
+    return render(request, 'blog/index.html', context={'contacts': contacts})
+
+
+def tags(request, pk):
+    cate = get_object_or_404(Tag, pk=pk)
+    post_list = Post.objects.filter(tags=cate).order_by('-created_time')
+    paginator = Paginator(post_list, 5)
+
+    page = request.GET.get('page')
+
+    try:
+        contacts = paginator.page(page)
+
+    except PageNotAnInteger:
+        contacts = paginator.page(1)
+
+    except EmptyPage:
+        contacts = paginator.page(paginator.num_pages)
+    return render(request, 'blog/index.html', context={'contacts': contacts})
